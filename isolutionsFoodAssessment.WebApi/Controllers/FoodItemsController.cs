dfdfa7df3198eb701cd1b615576ab60d050596cd
Javier Portaluppi <javier.portaluppi@isolutions.ch>
2022-02-11 14:23:16 +0100

    Remove post

--- isolutionsFoodAssessment.WebApi/Controllers/FoodItemsController.cs
+++ isolutionsFoodAssessment.WebApi/Controllers/FoodItemsController.cs
@@ -9,28 +9,22 @@ namespace isolutionsFoodAssessment.WebApi.Controllers
     [Route("[controller]")]
     public class FoodItemsController : ControllerBase
     {
-        private readonly AssessmentDbContext _assessmentDbContext;
+        private readonly AssessmentDbContext assessmentDbContext;
 
         public FoodItemsController(AssessmentDbContext assessmentDbContext)
         {
-            _assessmentDbContext = assessmentDbContext;
+            this.assessmentDbContext = assessmentDbContext;
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<FoodItem>))]
         public async Task<IActionResult> Get()
         {
-            var foodList = await _assessmentDbContext.FoodItems.ToListAsync();
+            var foodList = await assessmentDbContext.FoodItems.ToListAsync();
 
             return Ok(foodList);
         }
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(FoodItem))]
-        public async Task<IActionResult> Insert(object createFoodItem)
-        {
-            // ToDo: To be implemented by you
-            throw new NotImplementedException();
-        }
+        
     }
 }
